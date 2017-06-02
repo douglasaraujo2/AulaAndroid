4d2c63b02a787e4b82b568586a0e1036c8fe1349
Douglas Araujo <douglas@duxinnovation.com>
2017-06-02 14:07:48 -0300

    Criação da tela de pedidos

--- app/src/main/java/douglas/com/pizzariatradicao/LoginActivity.java
+++ app/src/main/java/douglas/com/pizzariatradicao/LoginActivity.java
@@ -1,13 +1,38 @@
 package douglas.com.pizzariatradicao;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout tilLogin;
+    private TextInputLayout tilSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tilLogin = (TextInputLayout) findViewById(R.id.tilLogin);
+        tilSenha = (TextInputLayout) findViewById(R.id.tilSenha);
+
+    }
+
+
+    public void logar(View view) {
+        String login = tilLogin.getEditText().getText().toString();
+        String senha = tilSenha.getEditText().getText().toString();
+        if (login.toLowerCase() == "douglas" && senha.toLowerCase() == "teste"){
+            Intent intent = new Intent(this,PedidosActivity.class);
+            intent.putExtra("usuario",login);
+            startActivity(intent);
+            finish();
+        }else {
+            tilLogin.setError("Login e/ou senha Inválidos");
+        }
+
+
     }
 }
